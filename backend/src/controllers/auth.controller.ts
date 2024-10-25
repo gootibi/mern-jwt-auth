0e094fefaeae877c0a1581ee06e9efec44fc2cc9
gootibi <gootibi@gmail.com>
2024-10-25 20:55:08 +0200

    Add register function (backend)

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,8 @@
 import { z } from "zod";
+import { CREATED } from "../constants/http";
+import { createAccount } from "../services/auth.service";
 import catchError from "../utils/catchError";
+import { setAuthCookies } from "../utils/cookies";
 
 const registerSchema = z.object({
     email: z.string().email().min(1).max(255),
@@ -21,7 +24,10 @@ export const registerHandler = catchError(async (req, res) => {
     })
 
     // call service
-    
+    const { user, accessToken, refreshToken } = await createAccount(request);
+
     // return response
-}
-);
+    setAuthCookies({ res, accessToken, refreshToken })
+        .status(CREATED)
+        .json(user);
+});
